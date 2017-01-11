39a73a2506f7b4bc1c0305f8f911953e3f670f84
spike91 <ivanpanas91@gmail.com>
2017-01-11 23:06:09 +0200

    sec

--- app/main.js
+++ app/main.js
@@ -6,5 +6,5 @@ platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1
 Copyright 2016 Google Inc. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at http://angular.io/license
-*/
+*/ 
 //# sourceMappingURL=main.js.map
